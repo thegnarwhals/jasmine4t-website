47d0933ad32e026fe3d2edf3205e3fe5f602adb5
jasmine <>
2024-07-05 22:44:13 +0100

    add contact page

--- main.js
+++ main.js
@@ -17,6 +17,33 @@ console.log("scale: ", scale)
 root.style.setProperty('--scale', scale);
 
 
+// Get the modal
+var modal = document.getElementById("contact-page");
+
+// Get the button that opens the modal
+var btn = document.getElementById("contact-button");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
 class SongkickWidgetProperties {
     constructor (iframe_scale_percentage, iframe_margin_top, upcoming_class_name) {
         this.iframe_scale_percentage = iframe_scale_percentage;
