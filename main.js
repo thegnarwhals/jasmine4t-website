bcff7fd6c082b375bc2ce870c0ff08957934a34e
jasmine <>
2024-07-05 16:44:45 +0100

    implement songkick scale selector

--- main.js
+++ main.js
@@ -1,11 +1,51 @@
 var root = document.documentElement;
 
-const unscaled_window_w = 320;
-const unscaled_window_h = 240;
-opt_scale_x = Math.floor(root.clientWidth / unscaled_window_w);
-opt_scale_y = Math.floor(root.clientHeight / unscaled_window_h);
-scale = Math.min(opt_scale_x, opt_scale_y);
+// Select optimal integer scale for pixel art
+const unscaled_window_w = 320;  // width of pixel art
+const unscaled_window_h = 240;  // height of pixel art
+const min_scale = 1;  // minimum scale for pixel art
+const max_scale = 4;  // maximum scale - very large monitors won't get more scaling than 4x
+client_width = root.clientWidth;
+client_height = root.clientHeight;
+
+opt_scale_x = Math.floor(client_width / unscaled_window_w);
+opt_scale_y = Math.floor(client_height / unscaled_window_h);
+opt_scale = Math.min(opt_scale_x, opt_scale_y);
+console.log("opt_scale: ", opt_scale)
+scale = Math.max(Math.min(opt_scale, max_scale), min_scale)
+console.log("scale: ", scale)
+scale = 4;
 root.style.setProperty('--scale', scale);
+
+
+class SongkickWidgetProperties {
+    constructor (iframe_scale_percentage, iframe_margin_top, upcoming_class_name) {
+        this.iframe_scale_percentage = iframe_scale_percentage;
+        this.iframe_margin_top = iframe_margin_top;  // Crop out songkick logo
+        this.upcoming_class_name = upcoming_class_name;
+    }
+}
+
+
+// Handtuned variables for various scales
+songkick_scale_selector = {
+    1: new SongkickWidgetProperties(80, -170, "upcoming-scale-1"),
+    2: new SongkickWidgetProperties(80, -146, "upcoming-scale-2"),
+    3: new SongkickWidgetProperties(80, -146, "upcoming-scale-3"),
+    4: new SongkickWidgetProperties(100, -91, "upcoming-scale-4"),
+}
+
+
+function setup_upcoming() {
+    songkick_iframe = document.querySelector('[id^=songkick-widget-]');
+    console.log("songkick_iframe_id: ", songkick_iframe.id);
+    properties = songkick_scale_selector[scale]
+    songkick_iframe.style.setProperty('scale', properties.iframe_scale_percentage + "%");
+    songkick_iframe.style.setProperty("margin-top", properties.iframe_margin_top + "px")
+    document.getElementById("upcoming").classList.add(properties.upcoming_class_name)
+}
+
+window.onload = setup_upcoming;
 
 document.getElementsByClassName("down-arrow")[0].addEventListener("click",
     event => document.getElementsByClassName("upcoming")[0].scrollBy({
